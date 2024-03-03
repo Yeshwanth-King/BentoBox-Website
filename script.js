a220c24d66634bdeb4adfec544e7158afe12c848
Yeshwanth-King <yeshuyeshwanth2005@gmail.com>
2024-03-03 08:37:18 +0530

    Currency Exchanger

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
-let userScore = 0;
-let botScore = 0;
-const userPoints = document.querySelector("#user-score");
-const botPoints = document.querySelector("#bot-score");
- const choices = document.querySelectorAll(".choice");
- const winnerDisplay = document.querySelector(".msg");
-const getCompChoice = () =>{
-    let options = ["rock","paper","scissors"];
-    let randomIdx = Math.floor(Math.random () * 3);
-    return options[randomIdx];
-}
+const baseURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropDown = document.querySelectorAll(".drop select");
+const getBtn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-const showWinner = (userWin, userChoice , compChoice) => {
-    if(userWin){
-        winnerDisplay.innerText = `You win!! Your ${userChoice} beats ${compChoice}`;
-        winnerDisplay.style.backgroundColor = "green";
-        userScore++;
-        userPoints.innerText = userScore;
+
+for (let select of dropDown){
+    for(code in countryList){
+        let newOptions = document.createElement("option");
+        if (select.name === "from" && code === "USD"){
+            newOptions.selected = "selected";
+        }
+        else if (select.name === "to" && code === "INR"){
+            newOptions.selected = "selected";
+        }
+        newOptions.innerText = code;
+        newOptions.value = code;
+        select.append(newOptions);
+
         
     }
-    else{
-        winnerDisplay.innerText = `You lose!! ${compChoice} beats Your ${userChoice}`;
-        winnerDisplay.style.backgroundColor = "red";
-        botScore++;
-        botPoints.innerText = botScore;
-    }
-}
-const drawGame = () =>{
-    winnerDisplay.innerText = "Game was draw, Play again!";
-    winnerDisplay.style.backgroundColor = "#081b31";
 
-}
-
- const playGame = (userChoice) =>{
-    console.log(`User choice was = ${userChoice}`);
-    let compChoice = getCompChoice();
-    console.log(`Computer choice was = ${compChoice}`);
-    if( userChoice === compChoice){
-        drawGame();
-    }
-    else{
-        let userWin = true;
-        if(userChoice === "rock"){
-            userWin = compChoice === "paper" ? false : true;
-
-        }
-        else if(userChoice === "paper"){
-            userWin = compChoice === "rock" ? true : false;
-        }
-        else if(userChoice === "scissors"){
-            userWin = compChoice === "rock" ? false : true;
-        }
-        showWinner(userWin,userChoice,compChoice);
-        
-    }
- }
-
- choices.forEach((choice) =>{
-    choice.addEventListener("click", () =>{
-        const userChoice = choice.getAttribute("id");
-        playGame(userChoice)
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target)
     })
- })
+}
+
+const updateFlag = (ele) =>{
+    let currCode = ele.value;
+    let countryCode = countryList[currCode];
+    let newsrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = ele.parentElement.querySelector("img");
+    img.src = newsrc;
+};
+
+getBtn.addEventListener("click", async(evt) =>{
+    evt.preventDefault();
+    let input = document.querySelector("form input");
+    let amtVal = input.value;
+    if( amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        input.value = 1;
+    }
+    console.log(amtVal);
+
+    console.log(fromCurr.value.toLowerCase())
+    console.log(toCurr.value.toLowerCase())
+
+    let URL = `${baseURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    console.log(rate);
+    
+    let finalAmt = amtVal * rate ;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt.value} ${toCurr}`;
+
+
+})
